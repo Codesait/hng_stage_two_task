76d56a7bbb7d87722942b8837c88a4ae0fd74f82
codesait <melasin4@gmail.com>
2021-08-17 17:45:41 +0100

    completed

---
+++ compulsory_task/namePrinterFunction.dart
@@ -0,0 +1,11 @@
+import 'package:stage_two_hng_task/Constants/constants.dart';
+
+class CompulsoryTask{
+
+  /// [printName] is a simple function for printing my name
+  /// and is currently ran in main.dart
+  static void printName() {
+    print('My name is $DEV_NAME');
+  }
+
+}
